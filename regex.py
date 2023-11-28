033f01f9499cbdca9e3191c1ea8b218807abe4dc
plestz <96513494+plestz@users.noreply.github.com>
2023-11-27 19:01:32 -0500

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,42 +1,29 @@
 import re
 
-
-def fill_in_doc(filename):
-    f = open(filename, "r")
-    string = f.readline()
-
-    doc = ""
-    while string:
-        doc += string
-        string = f.readline()
-
-    return doc
-
-
 class Registrar:
     def __init__(self, file):
         """
         :param file: the path to the file that you should read in
         """
-        self.document = fill_in_doc(file)
-        self.email_regex = r""  # TODO: determine what regex will match on emails
-        self.course_regex = r""  # TODO: determine what regex will match on course codes
+        self.document = read_to_string(file)
+        self.email_regex = r"" # TODO: determine what regex will match on emails
+        self.course_regex = r"" # TODO: determine what regex will match on course codes 
 
     def view_emails(self):
         """
-        Prints all of the matches with the email regex
-        :return: None; prints results
+        :return: all of the emails in the document
         """
         # TODO: find all of the email matches in self.document
-        pass
+        matches = []
+        return matches
 
     def view_courses(self):
         """
-        prints all of the matches with the course regex
-        :return: None; prints results
+        :return: all of the courses in the document
         """
         # TODO: find all of the course code matches in self.document!
-        pass
+        matches = []
+        return matches
 
     def make_changes(self, to_change, replacement):
         """
@@ -47,16 +34,19 @@ class Registrar:
         # TODO: write this method!
         pass
 
+def read_to_string(filename):
+    with open(filename, "r") as f:
+        return f.read()
 
 if __name__ == "__main__":
     registrar = Registrar("registrations.txt")
 
     # Try running your code with our provided text file and see if your results
     # look reasonable!
-    registrar.view_emails()
-    registrar.view_courses()
+    print(registrar.view_emails())
+    print(registrar.view_courses())
 
     registrar.make_changes(registrar.email_regex, "###")
-    registrar.view_emails()
+    print(registrar.view_emails())
 
     # Feel free to write your own tests as well!
