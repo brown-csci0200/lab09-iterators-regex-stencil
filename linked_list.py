46855f1da4bdfea38f244cb33744ccdc1d68050a
plestz <96513494+plestz@users.noreply.github.com>
2023-11-27 19:00:18 -0500

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,28 +1,18 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        self.prev = None
-
-
 class LinkedList:
     def __init__(self):
-        self.size = 0
         self.first = None
         self.last = None
+        self.size = 0
 
     def __iter__(self):
-        # TODO:
-        # 1. create a field in the class indicating which element to start iterating from, e.g. self.current = ...
-        # 2. return self
-        pass
+        # TODO: Call the constructor with the arguments you need.
+        return LinkedListIterator()
 
-    def __next__(self):
-        # TODO: write the next method for a LinkedList!
-        pass
+    def __len__(self):
+        return self.size
 
     def is_empty(self):
-        return self.first is None and self.last is None
+        return self.first is None
 
     def add_first(self, value):
         if self.is_empty():
@@ -34,6 +24,7 @@ class LinkedList:
             new_node.next = self.first
             self.first.prev = new_node
             self.first = new_node
+        self.size += 1
 
     def add_last(self, value):
         if self.is_empty():
@@ -45,17 +36,36 @@ class LinkedList:
             self.last.next = new_node
             new_node.prev = self.last
             self.last = new_node
+        self.size += 1
 
+class LinkedListIterator:
+    def __init__(self):
+        # TODO: set up the initial state (fields). 
+        # You will need to add parameters to this constructor.
+
+    def __next__(self):
+        # TODO: return the next item and update the state, 
+        # or raise `StopIteration` if there are no items left.
+        return None
+
+    def __iter__(self):
+        return self
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
 
 if __name__ == "__main__":
-    my_list = LinkedList()
-    my_list.add_first(5)
-    my_list.add_first(6)
-    my_list.add_first(7)
-    my_list.add_first(8)
-    my_list.add_last(4)
+    custom_list = LinkedList()
+    custom_list.add_first(5)
+    custom_list.add_first(6)
+    custom_list.add_first(7)
+    custom_list.add_first(8)
+    custom_list.add_last(4)
 
-    # when your iter and next methods work, this for loop
-    # should print the elements of  my_list!
-    for node in my_list:
-        print(node.value)
+    print('the final length is', len(custom_list))
+
+    for item in custom_list:
+        print(item)
